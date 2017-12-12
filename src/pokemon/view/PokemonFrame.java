57f5e994a14b335d01b9080e72a331b277405015
X1Warrior <zsto7638@gmail.com>
2017-12-12 13:02:53 -0700

    Set up the runner and the frame

--- src/pokemon/view/PokemonFrame.java
+++ src/pokemon/view/PokemonFrame.java
@@ -1,6 +1,32 @@
 package pokemon.view;
 
-public class PokemonFrame
+import javax.swing.JFrame;
+import pokemon.controller.PokemonController;
+import pokemon.view.PokemonPanel;
+
+public class PokemonFrame extends JFrame
 {
+	{
+		private PokemonController PokemonController;
+		private PokemonPanel PokemonPanel;
+		
+		public PokemonFrame(PokemonController PokemonController)
+		{
+			super();
+			this.PokemonController = PokemonController;
+			this.PokemonPanel = new PokemonPanel(PokemonController);
+			
+			setupFrame(); 
+			}
+		
+		private void setupFrame()
+		{
+			this.setContentPane(PokemonPanel);
+			this.setTitle("chatting with the chatbot");
+			this.setResizable(false);
+			this.setSize(600,600);
+			this.setVisible(true);
+		}
+	}
 
 }
