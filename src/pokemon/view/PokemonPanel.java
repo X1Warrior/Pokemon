c0eea0ce6542716e3305e0ec41a50252d03e4cbf
X1Warrior <zsto7638@gmail.com>
2017-12-18 13:54:41 -0700

    moved labels in gui and moved constraints to setup layout

--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -13,6 +13,12 @@ public class PokemonPanel extends JPanel
 	private JButton SaveButton;
 	private JButton LoadButton;
 	private JLabel infoLabel;
+	private JLabel infoLabel_1;
+	private JLabel infoLabel_2;
+	private JLabel infoLabel_3;
+	private JLabel infoLabel_4;
+	private JLabel infoLabel_5;
+	
 	private JScrollPane pokemonScrollPane;
 	
 	public PokemonPanel(PokemonController appController)
@@ -27,12 +33,16 @@ public class PokemonPanel extends JPanel
 		ResetButton = new JButton("Reset");
 		SaveButton = new JButton("Save");
 		LoadButton = new JButton("Load");
-		NumberLabel = new JLabel("#");
-		infoLabel = new JLabel("Name");
-		infoLabel = new JLabel("HealthPoints");
-		infoLabel = new JLabel("Attack");
-		infoLabel = new JLabel("Can evolve");
-		infoLabel = new JLabel("Enhancement");
+		infoLabel = new JLabel("#");
+		infoLabel_5 = new JLabel("Name");
+		infoLabel_4 = new JLabel("HealthPoints");
+
+		infoLabel_3 = new JLabel("Attack");
+
+		infoLabel_2 = new JLabel("Can evolve");
+
+		infoLabel_1 = new JLabel("Enhancement");
+
 		pokemonScrollPane = new JScrollPane();
 
 		
@@ -56,11 +66,11 @@ public class PokemonPanel extends JPanel
 		pokeArea.setEnabled(false);
 		pokeArea.setEditable(false);
 		this.add(LoadButton);
-		this.add(infoLabel);
-		this.add(NumberLabel);
-		this.add(infoLabel);
-		this.add(infoLabel);
-		this.add(infoLabel);
+		this.add(infoLabel_1);
+		this.add(infoLabel_2);
+		this.add(infoLabel_3);
+		this.add(infoLabel_4);
+		this.add(infoLabel_5);
 		this.add(infoLabel);
 	}
 	private void setupLayout()
@@ -71,6 +81,14 @@ public class PokemonPanel extends JPanel
 		appLayout.putConstraint(SpringLayout.WEST, SaveButton, 6, SpringLayout.EAST, ResetButton);
 		appLayout.putConstraint(SpringLayout.SOUTH, ResetButton, -10, SpringLayout.SOUTH, this);
 		appLayout.putConstraint(SpringLayout.EAST, ResetButton, -112, SpringLayout.EAST, this);
+		appLayout.putConstraint(SpringLayout.NORTH, infoLabel_4, 57, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.EAST, infoLabel_4, -84, SpringLayout.EAST, this);
+		appLayout.putConstraint(SpringLayout.EAST, infoLabel_1, -80, SpringLayout.EAST, this);
+		appLayout.putConstraint(SpringLayout.EAST, infoLabel_2, 0, SpringLayout.EAST, infoLabel_1);
+		appLayout.putConstraint(SpringLayout.NORTH, infoLabel_1, 0, SpringLayout.SOUTH, infoLabel_3);
+		appLayout.putConstraint(SpringLayout.SOUTH, infoLabel_2, -20, SpringLayout.NORTH, infoLabel_3);
+		appLayout.putConstraint(SpringLayout.SOUTH, infoLabel_3, -213, SpringLayout.SOUTH, this);
+		appLayout.putConstraint(SpringLayout.EAST, infoLabel_3, -84, SpringLayout.EAST, this);
 	}
 	private void setupListeners()
 	{
