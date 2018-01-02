e55c70fa45e5f63f7a119beb7260fe33f8ed9397
X1Warrior <zsto7638@gmail.com>
2018-01-02 13:40:17 -0700

    added constructors for ampharos

--- src/pokemon/model/Ampharos.java
+++ src/pokemon/model/Ampharos.java
@@ -2,9 +2,27 @@ package pokemon.model;
 
 public class Ampharos extends Pokemon implements Electric
 {
+	public Ampharos()
+	{
+		super(386, "Ampharos");
+	}
+	
+	public Ampharos(String name)
+	{
+		super(386, name);
+		setup();
+	}
 	public Ampharos(int number, String name)
 	{
 		super(number, name);
+		setup();
+	}
+	protected void setup()
+	{
+		this.setAttackPoints(250);
+		this.setCanEvolve(false);
+		this.setEnhancementModifier(5);
+		this.setHealthPoints(300);
 	}
 	public int electroBall(int strength)
 	{
